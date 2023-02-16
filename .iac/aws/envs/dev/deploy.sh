48594d145d5e76fd7d662d96ad01b784ae54a113
Carlos Soriano <soriano.carlitos@gmail.com>
2023-02-16 11:42:26 -0500

    fix index

--- .iac/aws/envs/dev/deploy.sh
+++ .iac/aws/envs/dev/deploy.sh
@@ -3,8 +3,9 @@
 set -e
 
 site_domain=$(terraform output -raw site_domain_dev)
+website_bucket_id=$(terraform output -json s3_dev | jq -r .website_bucket_id)
 repo_root=$(git rev-parse --show-toplevel)
 cd "$repo_root/app"
 # npm run build
 
-aws s3 cp --recursive --acl public-read --profile terraform-admin ./dist "s3://$site_domain"
+aws s3 cp --recursive --acl public-read --profile terraform-admin ./dist "s3://$website_bucket_id"
