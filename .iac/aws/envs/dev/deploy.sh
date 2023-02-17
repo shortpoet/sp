894c4646033d94c8aa390edc10d35d9e3625c707
Carlos Soriano <soriano.carlitos@gmail.com>
2023-02-17 12:49:40 -0500

    fix deploy script

--- .iac/aws/envs/dev/deploy.sh
+++ .iac/aws/envs/dev/deploy.sh
@@ -2,10 +2,14 @@
 
 set -e
 
+active_dir=$(pwd)
+script_dir=$(dirname "$0")
+cd "$script_dir"
+repo_root=$(git rev-parse --show-toplevel)
 site_domain=$(terraform output -raw site_domain_dev)
 website_bucket_id=$(terraform output -json s3_dev | jq -r .website_bucket_id)
-repo_root=$(git rev-parse --show-toplevel)
 cd "$repo_root/app"
-# npm run build
+npm run build
 
 aws s3 cp --recursive --acl public-read --profile terraform-admin ./dist "s3://$website_bucket_id"
+cd "$active_dir"
