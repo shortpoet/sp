f1a55327b92e0513089d35336752372b7221ff5b
Carlos Soriano <soriano.carlitos@gmail.com>
2023-02-17 14:57:14 -0500

    deploy script works with dev

--- .iac/aws/envs/dev/outputs.tf
+++ .iac/aws/envs/dev/outputs.tf
@@ -1,11 +1,11 @@
-output "site_domain_dev" {
+output "site_domain" {
   value = local.site_domain_dev
 }
 
-output "s3_dev" {
+output "s3" {
   value = module.s3_dev
 }
 
-output "cloudflare_dev" {
+output "cloudflare" {
   value = module.cloudflare_dev
 }
