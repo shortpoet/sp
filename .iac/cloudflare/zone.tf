6ae0095aeee9da5d200bcdfb58433e7388f35534
Carlos Soriano <soriano.carlitos@gmail.com>
2023-02-18 13:06:05 -0500

    cleanup

--- .iac/cloudflare/zone.tf
+++ .iac/cloudflare/zone.tf
@@ -2,14 +2,6 @@ data "cloudflare_accounts" "main" {
   name = "Soriano.carlos@gmail.com's Account"
 }
 
-# some other account... ?
-# accounts = {
-#       + accounts = []
-#       + id       = "d41d....."
-#       + name     = "soriano.carlos@gmail.com"
-#     }
-
-
 resource "cloudflare_zone" "shortpoet" {
   zone       = "shortpoet.com"
   account_id = data.cloudflare_accounts.main.accounts[0].id
