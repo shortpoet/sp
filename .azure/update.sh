4f5785f94545d67c46eaab3e64935d09ef61f865
Carlos Soriano <soriano.carlitos@gmail.com>
2023-05-28 14:16:01 -0400

    split dest_url

--- .azure/update.sh
+++ .azure/update.sh
@@ -7,10 +7,6 @@ dest_repo="Shortpoet"
 # dest_url="https://:$PAT@dev.azure.com/shortpoet/Shortpoet/_git/$dest_repo"
 # dest_url="https://$PAT@dev.azure.com/shortpoet/Shortpoet/_git/$dest_repo"
 # dest_url="https://$SYSTEM_ACCESSTOKEN@dev.azure.com/shortpoet/Shortpoet/_git/$dest_repo"
-dest_url="https://$SYSTEM_ACCESSTOKEN@dev.azure.com/shortpoet/Shortpoet/_git/$dest_repo"
-echo Starting the synchronization process
-echo "****Source Repo: $source_repo****"
-echo "****Destination Repo: $dest_url****"
 sourceURL="https://github.com/shortpoet/$source_repo"
 # git config --add remote.origin.fetch '+refs/heads/*:refs/heads/*'
 # git config --add remote.origin.fetch '+refs/tags/*:refs/tags/*'
@@ -18,12 +14,20 @@ sourceURL="https://github.com/shortpoet/$source_repo"
 
 b64_auth=true
 echo "***** Git clone Azure ****"
+echo Starting the synchronization process
+echo "****Source Repo: $source_repo****"
 if [[ $b64_auth ]]; then
+  dest_url="https://dev.azure.com/shortpoet/Shortpoet/_git/$dest_repo"
+  echo "****Destination Repo: $dest_url****"p
+
   B64_PAT=$(echo ":$SYSTEM_ACCESSTOKEN" | base64)
   echo "bg4: $B64_PAT"
   git config --global http.version HTTP/1.1
   git -c http.extraheader="AUTHORIZATION: Basic $B64_PAT" clone --bare "$dest_url"
 else
+  dest_url="https://$SYSTEM_ACCESSTOKEN@dev.azure.com/shortpoet/Shortpoet/_git/$dest_repo"
+  echo "****Destination Repo: $dest_url****"p
+
   git clone --bare "$dest_url"
 fi
 cd "$dest_repo.git" || exit
