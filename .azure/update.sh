a931f2879fe2ffb7efa7f5a4f3ea18efb2d23f5d
Carlos Soriano <soriano.carlitos@gmail.com>
2023-05-28 13:38:06 -0400

    no colon

--- .azure/update.sh
+++ .azure/update.sh
@@ -5,7 +5,7 @@ dest_repo="Shortpoet"
 # dest_url="https://shortpoet:$CPAT@dev.azure.com/shortpoet/Shortpoet/_git/$dest_repo"
 # dest_url="https://$PAT:PAT@dev.azure.com/shortpoet/Shortpoet/_git/$dest_repo"
 # dest_url="https://:$PAT@dev.azure.com/shortpoet/Shortpoet/_git/$dest_repo"
-dest_url="https://dev.azure.com/shortpoet/Shortpoet/_git/$dest_repo"
+dest_url="https://$PAT@dev.azure.com/shortpoet/Shortpoet/_git/$dest_repo"
 echo Starting the synchronization process
 echo "****Source Repo: $source_repo****"
 echo "****Destination Repo: $dest_url****"
