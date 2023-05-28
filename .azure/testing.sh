135d8e6a004869492bddd370c92f839e73b6495c
Carlos Soriano <soriano.carlitos@gmail.com>
2023-05-28 18:13:25 -0400

    test

--- .azure/testing.sh
+++ .azure/testing.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+
+testing=true
+again=true
+
+
+read -ra branches <<< "$(git for-each-ref --format='%(refname:short)' refs/heads/ | tr '\n' ' ')"
+echo "${branches[@]}"
+for branch in "${branches[@]}"; do
+  echo "**** Pulling $branch ****"
+done
