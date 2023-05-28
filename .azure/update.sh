64db3dcf2a1ee0cbf1f0a6553e26e90fcdbe0bbc
Carlos Soriano <soriano.carlitos@gmail.com>
2023-05-28 14:00:09 -0400

    b64

--- .azure/update.sh
+++ .azure/update.sh
@@ -6,14 +6,15 @@ dest_repo="Shortpoet"
 # dest_url="https://$PAT:PAT@dev.azure.com/shortpoet/Shortpoet/_git/$dest_repo"
 # dest_url="https://:$PAT@dev.azure.com/shortpoet/Shortpoet/_git/$dest_repo"
 # dest_url="https://$PAT@dev.azure.com/shortpoet/Shortpoet/_git/$dest_repo"
+# dest_url="https://$SYSTEM_ACCESSTOKEN@dev.azure.com/shortpoet/Shortpoet/_git/$dest_repo"
 dest_url="https://$SYSTEM_ACCESSTOKEN@dev.azure.com/shortpoet/Shortpoet/_git/$dest_repo"
 echo Starting the synchronization process
 echo "****Source Repo: $source_repo****"
 echo "****Destination Repo: $dest_url****"
 sourceURL="https://github.com/shortpoet/$source_repo"
-
-# git -c http.extraheader="AUTHORIZATION: bearer $PAT" clone --bare "$dest_url"
-git clone --bare "$dest_url"
+B64_PAT=$(echo ":$SYSTEM_ACCESSTOKEN" | base64)
+git -c http.extraheader="AUTHORIZATION: Basic $B64_PAT" clone --bare "$dest_url"
+# git clone --bare "$dest_url"
 cd "$dest_repo.git" || exit
 git remote add --mirror=fetch upstream "$sourceURL"
 git fetch upstream --tags
