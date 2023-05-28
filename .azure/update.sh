9ca8b6f8b36f4058f9634452f80b3bcbad70490e
Carlos Soriano <soriano.carlitos@gmail.com>
2023-05-28 13:33:59 -0400

    patpat

--- .azure/update.sh
+++ .azure/update.sh
@@ -3,13 +3,14 @@
 source_repo="sp"
 dest_repo="Shortpoet"
 # dest_url="https://shortpoet:$CPAT@dev.azure.com/shortpoet/Shortpoet/_git/$dest_repo"
-dest_url="https://$PAT:PAT@dev.azure.com/shortpoet/Shortpoet/_git/$dest_repo"
+# dest_url="https://$PAT:PAT@dev.azure.com/shortpoet/Shortpoet/_git/$dest_repo"
+dest_url="https://:$PAT@dev.azure.com/shortpoet/Shortpoet/_git/$dest_repo"
 echo Starting the synchronization process
 echo "****Source Repo: $source_repo****"
 echo "****Destination Repo: $dest_url****"
 sourceURL="https://github.com/shortpoet/$source_repo"
 
-git clone --bare "$dest_url"
+git -c http.extraheader="AUTHORIZATION: bearer $PAT" clone --bare "$dest_url"
 cd "$dest_repo.git" || exit
 git remote add --mirror=fetch upstream "$sourceURL"
 git fetch upstream --tags
